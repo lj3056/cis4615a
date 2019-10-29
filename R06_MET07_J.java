a5dd878be51a2743bb899692193b7b47fbac3150
Jacob <loraj@knights.ucf.edu>
2019-10-29 15:57:55 -0400

    java code

---
+++ R06_MET07_J.java
@@ -0,0 +1,63 @@
+/* Non Compliant */
+
+
+class GrantAccess {
+  public static void displayAccountStatus() {
+    System.out.println("Account details for admin: XX");
+  }
+}
+ 
+class GrantUserAccess extends GrantAccess {
+  public static void displayAccountStatus() {
+    System.out.println("Account details for user: XX");
+  }
+}
+ 
+public class StatMethod {
+  public static void choose(String username) {
+    GrantAccess admin = new GrantAccess();
+    GrantAccess user = new GrantUserAccess();
+    if (username.equals("admin")) {
+      admin.displayAccountStatus();
+    } else {
+      user.displayAccountStatus();
+    }
+  }
+ 
+  public static void main(String[] args) {
+    choose("user");
+  }
+}
+
+/* Compliant */
+
+
+class GrantAccess {
+  public void displayAccountStatus() {
+    System.out.print("Account details for admin: XX");
+  }
+}
+ 
+class GrantUserAccess extends GrantAccess {
+  @Override
+  public void displayAccountStatus() {
+    System.out.print("Account details for user: XX");
+  }
+}
+ 
+public class StatMethod {
+  public static void choose(String username) {
+    GrantAccess admin = new GrantAccess();
+    GrantAccess user = new GrantUserAccess();
+ 
+    if (username.equals("admin")) {
+      admin.displayAccountStatus();
+    } else {
+      user.displayAccountStatus();
+    }
+  }
+ 
+  public static void main(String[] args) {
+    choose("user");
+  }
+}
