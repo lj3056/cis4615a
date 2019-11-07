a148f10a263f7ad6a53571ef3efb69e8e14df6b4
Jacob <loraj@knights.ucf.edu>
2019-11-07 12:38:54 -0500

    Compiles

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,16 +1,17 @@
-/* Non Compliant */
+public class R04_STR03_J {
+	public static void main(String args[]) {
+		System.out.println("Data properly casted to string");
+		CompliantSolution();
 
-BigInteger x = new BigInteger("530500452766");
-byte[] byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+	
+	}
+public static void CompliantSolution(){
+	BigInteger x = new BigInteger("530500452766");
+	String s = x.toString(); //Valid character data
+	byte[] byteArray = s.getBytes();
+	String ns = new String(byteArray);
+	x = new BigInteger(ns);
+	System.out.println("x = " + x);
+	}
 
-
-/* Compliant */
-
-BigInteger x = new BigInteger("530500452766");
-String s = x.toString();  // Valid character data
-byte[] byteArray = s.getBytes();
-String ns = new String(byteArray); 
-x = new BigInteger(ns);
+}
